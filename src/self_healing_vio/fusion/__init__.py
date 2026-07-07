97a0b0a26be4852c884b961118609d48698fd7e8
Panagiota Grosdouli <75089541+panagiotagrosdouli@users.noreply.github.com>
2026-07-07 11:22:01 +0300

    Export health fusion utility

--- src/self_healing_vio/fusion/__init__.py
+++ src/self_healing_vio/fusion/__init__.py
@@ -1,5 +1,6 @@
 """Fusion interfaces for confidence-aware VIO integration."""
 
+from .health_fusion import HealthFusion
 from .interfaces import HealthAwareState, VIOBackendAdapter
 
-__all__ = ["HealthAwareState", "VIOBackendAdapter"]
+__all__ = ["HealthFusion", "HealthAwareState", "VIOBackendAdapter"]
