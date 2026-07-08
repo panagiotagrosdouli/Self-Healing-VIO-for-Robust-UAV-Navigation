8458f1be0f183cd7538a8189d83cc8ecdd6048df
Panagiota Grosdouli <75089541+panagiotagrosdouli@users.noreply.github.com>
2026-07-08 09:58:36 +0300

    Add ROS2 launch skeleton

---
+++ launch/health_monitor.launch.py
@@ -0,0 +1,12 @@
+"""Planned ROS2 launch skeleton for SHIELD-VIO.
+
+This file documents the intended launch structure. It is not expected to run
+until the ROS2 package and node executables are implemented.
+"""
+
+from launch import LaunchDescription
+
+
+def generate_launch_description():
+    """Return an empty launch description until ROS2 nodes are implemented."""
+    return LaunchDescription([])
